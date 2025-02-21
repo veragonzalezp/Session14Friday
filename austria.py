f7bf56ba622707adcac2c83b3975a4b48a6d1b73
veragonzalez <veragonzalez2014@gmail.com>
2025-02-21 13:17:07 +0100

    Initial Commit

---
+++ austria.py
@@ -0,0 +1,4 @@
+def greet():
+    print("Hallo")
+def cook():
+     print("we are making shnitzel")
---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+import argentina
+import austria
+import brazil
+import china
+import peru
+
+print ("a random number is:", random.randint(1, 10))
+china.cook()
+china.greet()
+austria.cook()
+austria.greet()
+argentina.cook()
+brazil.cook()
+peru.cook()
